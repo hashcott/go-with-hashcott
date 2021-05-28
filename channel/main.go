331342f9adc08ce1091b739f81629468acdd10b2
fungerouscode <duchanhstyle@gmail.com>
2021-05-28 15:26:47 +0700

    select multi channel

--- channel/main.go
+++ channel/main.go
@@ -7,18 +7,44 @@ import (
 
 func main() {
 	var wg = sync.WaitGroup{}
-	ch := make(chan int)
+	ch1 := make(chan int, 50)
+	ch2 := make(chan int, 50)
 
 	wg.Add(2)
-	go func(ch <-chan int) {
-		i := <-ch
-		fmt.Println(i)
+	go func() {
+		/*
+			Duyet channel bang for...range
+			for i := range ch {
+			 	fmt.Println(i)
+			}
+		*/
+		/*
+			Cach 2
+			for {
+				if i, ok := <-ch; ok {
+					fmt.Println(i)
+				} else {
+					break
+				}
+			}
+		*/
+		select {
+		case i := <-ch1:
+			fmt.Println(i)
+		case j := <-ch2:
+			fmt.Println(j)
+		default:
+			fmt.Println("Default case executed")
+		}
 		wg.Done()
-	}(ch)
+	}()
 
-	go func(ch chan<- int) {
-		ch <- 42
+	go func() {
+		ch1 <- 42
+		close(ch1)
+		ch2 <- 27
+		close(ch2)
 		wg.Done()
-	}(ch)
+	}()
 	wg.Wait()
 }
